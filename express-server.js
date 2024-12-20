060108e2115428cbc1a089f1cf7859b4910fde94
Salma-Deen <165559454+Salma-Deen@users.noreply.github.com>
2024-12-21 03:45:24 +0530

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import roomsRouter from "./routes/rooms.js";
 import custRouter from "./routes/customer.js";
-import bookRouter from "./routes/bookings.js";
+import bookRouter from "./routes/booking.js";
 const server = express();
 server.use(express.json());
 
 server.use("/rooms", roomsRouter);
 server.use("/customer", custRouter);
-server.use("/booking",bookRouter)
-const PORT = 7000;
+server.use("/booking", bookRouter)
+const PORT = 4500;
 
 server.listen(PORT, () => {
     console.log("Server listening on ", PORT);
